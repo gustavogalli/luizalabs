162c57c9d4947fc6e4e14f23aac00093cc18df0d
gustavogalli <gustavosgalli@gmail.com>
2025-05-14 21:00:52 -0300

    [feat] Criando OrderService

---
+++ src/main/java/com/galli/luizalabs/service/OrderService.java
@@ -0,0 +1,14 @@
+package com.galli.luizalabs.service;
+
+import com.galli.luizalabs.dto.UserResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface OrderService {
+    List<UserResponse> processFile(MultipartFile file) throws IOException;
+
+    List<UserResponse> getFilteredOrders(Long orderId, LocalDate startDate, LocalDate endDate);
+}
